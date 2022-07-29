ea052b5d3da4365cdad4a4fe63abc261fb0e1dae
MR-HENDRA <hunterblackshadow26@gmail.com>
2022-07-29 15:57:50 +0800

    Operator Penugasan

---
+++ assets/operatorPenugasan.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = 5;
+
+x += y;
+
+console.log(x);
+
+/* output
+15
+*/
+
+let x2 = 10;
+let y2 = 5;
+
+x += y; // artinya -> x = x + y;
+x -= y; // artinya -> x = x - y;
+x *= y; // artinya -> x = x * y;
+x /= y; // artinya -> x = x / y;
+x %= y; // artinya -> x = x % y;
